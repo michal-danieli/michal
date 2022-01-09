14a537258ef7f417e8cf9de5a2331c2eb0ed8977
michal <dakya.danieli@gmail.com>
2022-01-09 23:02:09 +0200

    home component

--- src/HomePage.js
+++ src/HomePage.js
@@ -5,13 +5,45 @@ import axios from "axios";
 
 class homePage extends React.Component {
     state = {
+        sales: []
+    }
 
+    componentDidMount() {
+        this.getUserSales()
+    }
+
+    getUserSales =()=>{
+        const cookies = new Cookies();
+        axios.get("http://localhost:8989/get-user-sales",{
+            params:{
+                token: cookies.get("logged_in")
+            }
+        })
+            .then((response)=>{
+                let sales = response.data
+                this.setState({
+                    sales:sales
+                })
+            })
     }
 
     render() {
         return (
             <div>
                 this is the home page
+                <div>
+                    {
+                        this.state.sales.map(sale =>{
+                            return(
+                                <div>
+                                    shop: {sale.shop.name}
+                                    description: {sale.description}
+                                </div>
+                            )
+                          }
+                        )
+                    }
+                </div>
             </div>
         )
     }
