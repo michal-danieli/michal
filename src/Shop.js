c0d77bbf5fdefe3634102204aaebf3a84d6ccccb
michal <dakya.danieli@gmail.com>
2022-01-09 22:22:40 +0200

    shop component

--- src/Shop.js
+++ src/Shop.js
@@ -5,20 +5,77 @@ import axios from "axios";
 
 class Shop extends React.Component {
     state = {
-        id: ""
+        id: 0,
+        sales : [],
+        shop:{}
     }
     componentDidMount() {
         const id =this.props.match.params.id;
         this.setState({
             id:id
         })
+        this.getShopById()
+        this.getAllSalesOfShop()
+    }
+
+    getShopById = ()=>{
+        axios.get("http://localhost:8989/get-shop-by-id",{
+            params:{
+                shopId: parseInt(this.state.id)
+            }
+        })
+            .then((response)=>{
+                const shop = response.data
+                this.setState({
+                    shop:shop
+                })
+            })
+    }
+
+
+
+    getAllSalesOfShop = () =>{
+        axios.get("http://localhost:8989/get-all-sales-of-shop",{
+            params:{
+                shopId: parseInt(this.state.id)
+            }
+        })
+            .then((response)=>{
+                const sales = response.data
+                this.setState({
+                    sales:sales
+                })
+            })
     }
 
     render() {
         return (
             <div>
                 this is the shop page
-                {this.state.id}
+                <div>
+                    shop name:
+                </div>
+                <p></p>
+                {
+                    this.state.shop.name
+                }
+                <p>
+                    {
+                        this.state.id
+                    }
+                </p>
+                {
+                    this.state.sales.map(sale =>{
+                        return(
+                            <div>
+                                {sale.name}
+                                <p></p>
+                                {sale.description}
+                            </div>
+                        )
+                    })
+                }
+
             </div>
         )
     }
